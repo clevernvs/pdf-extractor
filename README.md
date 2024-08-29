785bcea9724c7ad9abed695c036f8157a237ab18
Cleverton Neves <clever.nvs@gmail.com>
2024-08-28 21:41:50 -0300

    feat: Iniciando o projeto

---
+++ README.md
@@ -0,0 +1,18 @@
+<div align="center">  
+
+<h1> App Python Llama Parse PDF </h1>
+
+![Python](https://img.shields.io/badge/-Python-1e272e?style=for-the-badge&logo=Python)&nbsp;
+</div>
+
+<h1> Descrição: </h1>
+<p>
+Aplicação para leiturar e extração de conteúdo de um arquivo em PDF
+</p>
+
+<h1> Funções básicas: </h1>
+<ul>
+<li>Ler um arquivo em PDF</li>
+<li>Extrair textos do arquivo em PDF</li>
+<li>Extrair tabelas do arquivo em PDF</li>
+</ul>
---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+os.environ["LHAMA_CLOUD_API_KEY"] = "llx-sw4tyjcYJtflJ311Sz5q2ibmMjGG0521I0BGc5Mzztg3SIT4"
+
+from llama_parse import LlamaParse
+
+documents = LlamaParse(result_type="markdown", parsing_instruction="This file contains texts and tables. I'd like to get only the tables from the text").load_data("resultado.pdf")
+
+print(len(documents))
+
+for i, page in enumerate(documents):
+    with open(f"my_pdf/pagina{i+1}.md", "w", encoding="utf-8") as archive:
+        archive.write(page.text)
